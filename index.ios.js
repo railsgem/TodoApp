930eb56bd56984749cd96073e42104170e2281cc
railsgem <jun.chenying@hotmail.com>
2016-12-18 14:56:47 +1100

    list view

--- index.ios.js
+++ index.ios.js
@@ -5,13 +5,19 @@
  */
 
 import React, { Component } from 'react';
-const styles = require('./styles.js')
+const styles = require('./styles.js');
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
 
 import {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  AlertIOS,
 } from 'react-native';
 import * as firebase from 'firebase';
 
@@ -26,15 +32,100 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class TodoApp extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        dataSource: new ListView.DataSource({
+          rowHasChanged: (row1, row2) => row1 !== row2,
+        })
+      };
+      this.itemsRef = this.getRef().child('items');
+    }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          I’m a container lol!
-        </Text>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+
       </View>
+    )
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  _addItem() {
+    AlertIOS.prompt(
+      'Add New Item',
+      null,
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      'plain-text'
     );
   }
+
+  _renderItem(item) {
+
+    const onPress = () => {
+      AlertIOS.alert(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
+    );
+  }
+
 }
+
 
 AppRegistry.registerComponent('TodoApp', () => TodoApp);
