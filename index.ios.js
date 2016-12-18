7a9ad7ea7f713df047942ae7aeb3f331b4d29c39
railsgem <jun.chenying@hotmail.com>
2016-12-18 13:57:17 +1100

    init

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,36 @@
  */
 
 import React, { Component } from 'react';
+const styles = require('./styles.js')
+
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ6XnUZ4V8SKN7SFalNJkof7LEh4eaVSI",
+  authDomain: "mytodo-6fe56.firebaseapp.com",
+  databaseURL: "https://mytodo-6fe56.firebaseio.com",
+  storageBucket: "mytodo-6fe56.appspot.com",
+  messagingSenderId: "79226061602"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class TodoApp extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          I’m a container lol!
         </Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('TodoApp', () => TodoApp);
